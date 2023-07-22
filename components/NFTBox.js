df1c008aa5f5552e2d68a63fc622b519020f72c7
Naresh kumar <nareshxo@Nareshs-Air.wind3.hub>
2023-07-22 23:19:52 +0200

    final commit

---
+++ components/NFTBox.js
@@ -0,0 +1,149 @@
+import { useState, useEffect } from "react"
+import { useWeb3Contract, useMoralis } from "react-moralis"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import nftAbi from "../constants/BasicNft.json"
+import Image from "next/image"
+import { Card, useNotification } from "web3uikit"
+import { ethers } from "ethers"
+import UpdateListingModal from "./UpdateListingModal"
+
+//the Nft Box is where we gon grab all the info(activeItemBft's price, sellerid, tokenId) show all the info about our activeItemNft
+const truncateStr = (fullStr, strLen) => {
+    if (fullStr.length <= strLen) return fullStr
+
+    const separator = "..."
+    const seperatorLength = separator.length
+    const charsToShow = strLen - seperatorLength
+    const frontChars = Math.ceil(charsToShow / 2)
+    const backChars = Math.floor(charsToShow / 2)
+    return (
+        fullStr.substring(0, frontChars) +
+        separator +
+        fullStr.substring(fullStr.length - backChars)
+    )
+}
+
+export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {
+    const { isWeb3Enabled, account } = useMoralis()
+    const [imageURI, setImageURI] = useState("")
+    const [tokenName, setTokenName] = useState("")
+    const [tokenDescription, setTokenDescription] = useState("")
+    const [showModal, setShowModal] = useState(false)
+    const hideModal = () => setShowModal(false)
+    const dispatch = useNotification()
+
+    const { runContractFunction: getTokenURI } = useWeb3Contract({
+        abi: nftAbi,
+        contractAddress: nftAddress,
+        functionName: "tokenURI",
+        params: {
+            tokenId: tokenId,
+        },
+    })
+
+    const { runContractFunction: buyItem } = useWeb3Contract({
+        abi: nftMarketplaceAbi,
+        contractAddress: marketplaceAddress,
+        functionName: "buyItem",
+        msgValue: price,
+        params: {
+            nftAddress: nftAddress,
+            tokenId: tokenId,
+        },
+    })
+
+    async function updateUI() {
+        //In order to get our nft Image --> 1. we ve to get the tokenUri and 2. using the image tag from the tokenUri gets the image
+        const tokenURI = await getTokenURI()
+        console.log(`The TokenURI is ${tokenURI}`)
+        // We are going to cheat a little here...
+        if (tokenURI) {
+            //since not all the browser are ipfs companion we'l make a change to its ipfs edition to https edition. which is Ipfs Gateway
+            // IPFS Gateway: A server that will return IPFS files from a "normal" URL.
+            const requestURL = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/") // replacing the ipfs to https compatible(gateway)
+            const tokenURIResponse = await (await fetch(requestURL)).json()
+            const imageURI = tokenURIResponse.image
+            const imageURIURL = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/") //lly for the img url
+            setImageURI(imageURIURL)
+            setTokenName(tokenURIResponse.name)
+            setTokenDescription(tokenURIResponse.description)
+            // We could render the Image on our sever, and just call our sever.
+            // For testnets & mainnet -> use moralis server hooks
+            // Have the world adopt IPFS
+            // Build our own IPFS gateway
+        }
+        // get the tokenURI
+        // using the image tag from the tokenURI, get the image
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const isOwnedByUser = seller === account || seller === undefined
+    const formattedSellerAddress = isOwnedByUser ? "you" : truncateStr(seller || "", 15) //if its not us, truncate str of seller or blank str of size 15
+
+    const handleCardClick = () => { //this is gon update the var for whether or not we should show this modal
+        isOwnedByUser
+            ? setShowModal(true)
+            : buyItem({
+                  onError: (error) => console.log(error),
+                  onSuccess: handleBuyItemSuccess,
+              })
+    }
+
+    const handleBuyItemSuccess = async (tx) => {
+        await tx.wait(1)
+        dispatch({
+            type: "success",
+            message: "Item bought!",
+            title: "Item Bought",
+            position: "topR",
+        })
+    }
+
+    return (
+        <div>
+            <div>
+                {imageURI ? (
+                    <div>
+                        <UpdateListingModal // we ve to tell our modal to be visible only when someone clicks our nft/box/card by handleCardClick()
+                            isVisible={showModal} 
+                            tokenId={tokenId}
+                            marketplaceAddress={marketplaceAddress}
+                            nftAddress={nftAddress}
+                            onClose={hideModal}
+                        />
+                        <Card
+                            title={tokenName}
+                            description={tokenDescription}
+                            onClick={handleCardClick}
+                        >
+                            <div className="p-2">
+                                <div className="flex flex-col items-end gap-2">
+                                    <div>#{tokenId}</div>
+                                    <div className="italic text-sm">
+                                        Owned by {formattedSellerAddress}
+                                    </div>
+                                    <Image
+                                        loader={() => imageURI}
+                                        src={imageURI}
+                                        height="200"
+                                        width="200"
+                                    />
+                                    <div className="font-bold">
+                                        {ethers.utils.formatUnits(price, "ether")} ETH
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
+        </div>
+    )
+}
